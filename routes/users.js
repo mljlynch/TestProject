3ee9cf842290a1710edbdd3f3addd0674fc45195
Jacob Lynch <jacoblynch@Jacobs-MacBook-Pro.local>
2017-07-09 17:42:34 -0700

    updated README

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
+  router.get('/users', function(req, res, next) {
   res.render('users', {tile: 'users'})
 });
 
 module.exports = router;
+  
